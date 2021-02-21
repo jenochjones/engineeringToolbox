73b22e12cabe952e0ba3e77a304e88d3f2b15b69
jenochjones <jon.enoch.jones@gmail.com>
2021-02-21 14:44:55 -0700

    files

---
+++ public/js/engineeringTools.js
@@ -0,0 +1,9 @@
+function linearInterpolation () {
+    let x1 = parseFloat($('#x1-input').val());
+    let x2 = parseFloat($('#x2-input').val());
+    let x3 = parseFloat($('#x3-input').val());
+    let y1 = parseFloat($('#y1-input').val());
+    let y3 = parseFloat($('#y3-input').val());
+    let y2 = y1 + ((x2 - x1) * (y3 - y1) / (x3 - x1));
+    $('#interp-result').empty().append(y2.toPrecision(3));
+}
