64e47087c4422e280fd886faf5ffaf17573c361b
jenochjones <jon.enoch.jones@gmail.com>
2021-02-21 17:21:58 -0700

    add navbar

--- public/js/engineeringTools.js
+++ public/js/engineeringTools.js
@@ -1,3 +1,10 @@
+function showLinearInterpolation () {
+    $('.tool').each(function () {
+        $(this).css('display', 'none');
+    });
+    $('#linear-interp').css('display', 'flex');
+}
+
 function linearInterpolation () {
     let x1 = parseFloat($('#x1-input').val());
     let x2 = parseFloat($('#x2-input').val());
@@ -6,4 +13,10 @@ function linearInterpolation () {
     let y3 = parseFloat($('#y3-input').val());
     let y2 = y1 + ((x2 - x1) * (y3 - y1) / (x3 - x1));
     $('#interp-result').empty().append(y2.toPrecision(3));
+}
+
+function showNone () {
+    $('.tool').each(function () {
+        $(this).css('display', 'none');
+    });
 }
