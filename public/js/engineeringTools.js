cb84aa2fc378f11ce5c6de5c091369c6921a18fd
jenochjones <jon.enoch.jones@gmail.com>
2021-02-28 12:21:26 -0700

    Finished flow from depth

--- public/js/engineeringTools.js
+++ public/js/engineeringTools.js
@@ -27,44 +27,62 @@ function showNormalAndCriticalDepth() {
 }
 
 function normalAndCriticalDepth() {
-    let a, r, i, theta;
+    if ($('#enter-depth-radio').prop('checked') == true) {
+        let y = parseFloat($('#enter-depth').val());
+        let flow = solveForAandR(y);
+        $('#enter-flow').val(Math.round(flow * 1000) / 1000);
+    } else if ($('#enter-flow-radio').prop('checked') == true) {
+        let finalFlow = $('#enter-flow').val();
+        let yn = 0.0001;
+        let tryFlow = 0;
+        console.log('final flow: ' + finalFlow)
+        debugger
+        while (tryFlow > finalFlow + 0.00001 || tryFlow < finalFlow - 0.00001) {
+            yn += (finalFlow - tryFlow) / (finalFlow * 6);
+            tryFlow = solveForAandR(yn);
+            console.log('tryflow: ' + tryFlow)
+        }
+        $('#enter-depth').val(Math.round(yn * 1000) / 1000);
+    }
+}
+
+function solveForAandR(y) {
+    let a, r, i, theta, flow;
     let z1 = parseFloat($('#side-slope1').val());
     let z2 = parseFloat($('#side-slope2').val());
     let b = parseFloat($('#channel-width').val());
     let d = parseFloat($('#pipe-diameter').val());
     let lo = parseFloat($('#longitudinal-slope').val());
     let n = parseFloat($('#mannings-roughness').val());
-    if ($('#enter-depth-radio').prop('checked') == true) {
-        let y = parseFloat($('#enter-depth').val());
-        if ($('#nc-units').val() == 'Metric') {
-            i = 1;
-        } else {
-            i = 1.49;
-        }
-        if ($('#nc-channel-type').val() == 'Trapezoidal') {
-            a = (b + (z1 * y / 2) + (z2 * y / 2)) * y;
-            r = a / (b + y * Math.sqrt(z1 ^ 2 + 1))
-        } else if ($('#nc-channel-type').val() == 'Rectangular') {
-            a = b * y;
-            r = a / (2 * y + b);
-            solveManningsForFlow (a, r, i, n, lo);
-        } else if ($('#nc-channel-type').val() == 'Triangular') {
-        } else if ($('#nc-channel-type').val() == 'Circular') {
-            a = d ^ 2 * Math.PI / 4;
-            theta = 2 * Math.PI - 2 * Math.acos((y - d / 2) / (d / 2));
-
-        } else if ($('#nc-channel-type').val() == 'Cross Section') {
-
-        }
+    if ($('#nc-units').val() == 'Metric') {
+        i = 1;
+    } else {
+        i = 1.49;
     }
+    if ($('#nc-channel-type').val() == 'Trapezoidal') {
+        a = (b + (z1 * y / 2) + (z2 * y / 2)) * y;
+        r = a / (b + y * Math.sqrt(Math.pow(z1, 2) + 1) + y * Math.sqrt(Math.pow(z2, 2) + 1));
+    } else if ($('#nc-channel-type').val() == 'Rectangular') {
+        a = b * y;
+        r = a / (2 * y + b);
+    } else if ($('#nc-channel-type').val() == 'Triangular') {
+        a = ((z1 * y / 2) + (z2 * y / 2)) * y;
+        r = a / (y * Math.sqrt(Math.pow(z1, 2) + 1) + y * Math.sqrt(Math.pow(z2, 2) + 1));
+    } else if ($('#nc-channel-type').val() == 'Circular') {
+        theta = 2 * (Math.PI - Math.acos((2 * y / d) - 1));
+        a = (theta - Math.sin(theta)) * Math.pow(d, 2) / 8;
+        r = (1 - Math.sin(theta) / theta) * d / 4;
+    } else if ($('#nc-channel-type').val() == 'Cross Section') {
 
+    }
+    flow = solveManningsForFlow (a, r, i, n, lo);
+    return flow;
 }
 
 function solveManningsForFlow (a, r, i, n, s) {
-    let flow = (i / n) * a * r ^ 2/3 * Math.sqrt(s);
-    console.log(flow)
+    let flow = (i / n) * a * Math.pow(r, 2/3) * Math.sqrt(s);
+    return flow
 }
-
 
 function setChannelType() {
     let sideSlope1 = true;
